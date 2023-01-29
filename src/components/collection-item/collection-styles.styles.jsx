acc70619e3d3ae80b821c0eb0540a056ff56a50a
venki200221 <venki21122@gmail.com>
2023-01-29 12:57:05 +0530

    initail commit

---
+++ src/components/collection-item/collection-styles.styles.jsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import CustomButton from '../custom-button/custom-button.component';
+
+export const CollectionItemContainer = styled.div`
+  width: 22vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  background: #fff;
+    box-shadow: 0 6px 10px rgba(0, 0, 0, .08), 0 0 6px rgba(0, 0, 0, .05);
+    transition: .3s transform cubic-bezier(.155, 1.105, .295, 1.12), .3s box-shadow, .3s -webkit-transform cubic-bezier(.155, 1.105, .295, 1.12);
+    border: 0;
+    border-radius: 1rem;
+  align-items: center;
+  position: relative;
+  &:hover {
+    .image {
+      opacity: 0.8;
+    }
+    button {
+      opacity: 0.85;
+      display: flex;
+    }
+  }
+`;
+
+export const AddButton = styled(CustomButton)`
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 255px;
+  display: none;
+`;
+
+export const BackgroundImage = styled.div`
+  width: 100%;
+  height: 75%;
+  background-size: cover;
+  background-position: center;
+  border-top-left-radius: calc(1rem - 1px);
+  border-top-right-radius: calc(1rem - 1px);
+  margin-bottom: 5px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+`;
+
+export const CollectionFooterContainer = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+`;
+
+export const NameContainer = styled.span`
+  width: 90%;
+  margin-bottom: 15px;
+`;
+
+export const PriceContainer = styled.span`
+  width: 10%;
+  text-align: right;
+`;
